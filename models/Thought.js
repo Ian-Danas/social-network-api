8c160158b1185ebaf7ee4a68d7a620dc76235168
Ian Danas <ianmdanas@gmail.com>
2023-05-23 09:44:33 -0700

    adding reactionID to reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,11 +1,11 @@
-const { Schema, model,ObjectId} = require('mongoose');
+const { Schema, model,ObjectId,Types} = require('mongoose');
 
 
 const reactionSchema = new Schema({
-  // reactionId: { 
-  //   type: Schema.Types.ObjectId, 
-  //   default: new Schema.Types.ObjectId()
-  // },
+  reactionId: { 
+    type: Schema.Types.ObjectId, 
+    default: () => new Types.ObjectId()
+  },
   reactionBody: {
     type:String,
     required:true,
