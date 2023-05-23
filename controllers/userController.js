1d6908e5421bb300ac1812686ebc7e8cef5f628a
Ian Danas <ianmdanas@gmail.com>
2023-05-22 17:52:53 -0700

    addiing all routes for users

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,4 +23,34 @@ module.exports = {
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.status(500).json(err));
   },
+  updateUser(req, res){
+    // Uses findOneAndUpdate() method on model
+    User.findOneAndUpdate(
+      {_id: req.params.userId },
+      req.body,
+      // Sets to true so updated document is returned; Otherwise original document will be returned
+      { new: true },
+      (err, result) => {
+        if (result) {
+          res.status(200).json(result);
+          console.log(`Updated: ${result}`);
+        } else {
+          console.log('Uh Oh, something went wrong');
+          res.status(500).json({ message: 'something went wrong' });
+        }
+      }
+    );
+  },
+
+  deleteUser(req, res){
+    User.findOneAndDelete({ _id: req.params.userId }, (err, result) => {
+      if (result) {
+        res.status(200).json(result);
+        console.log(`Deleted: ${result}`);
+      } else {
+        console.log('Uh Oh, something went wrong');
+        res.status(500).json({ message: 'something went wrong' });
+      }
+    });
+  },
 };
